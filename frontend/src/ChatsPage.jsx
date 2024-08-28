3f4ee104ed813a216cf492bf860a9fc63d8f22b2
unknown <shinlee7878@gmail.com>
2024-08-29 00:06:27 +0900

    fix readme

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,9 +4,9 @@ const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh" }}>
       <PrettyChatWindow
-        projectId="57273819-9d50-4adc-bb7d-eee4978964de" // 프로젝트 ID
-        username={props.user.username} // 사용자 이름
-        secret={props.user.secret} // 사용자 비밀번호 또는 시크릿
+        projectId="57273819-9d50-4adc-bb7d-eee4978964de" 
+        username={props.user.username} 
+        secret={props.user.secret} 
         style={{ height: "100%" }}
       />
     </div>
